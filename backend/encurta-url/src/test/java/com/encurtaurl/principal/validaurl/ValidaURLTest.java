43f864e56a028b34c8f80e41686e7436c7f82643
Marcos Camargo <mp.camargo@outlook.com>
2025-11-16 13:34:03 -0300

    feat: Implementados testes unitários da ValidaURL
    - Implementados testes unitários para reconhecer URLs válidas e
      inválidas:
      - URL padrão
      - URL com query params
      - URL com protocolo HTTP
      - URL sem protocolo
      - URL sem o domínio de nível superior

---
+++ backend/encurta-url/src/test/java/com/encurtaurl/principal/validaurl/ValidaURLTest.java
@@ -0,0 +1,53 @@
+package com.encurtaurl.principal.validaurl;
+
+import com.encurtaurl.principal.api.validacao.url.ValidaURLImpl;
+import jakarta.validation.ConstraintValidatorContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ValidaURLTest {
+
+    private final ValidaURLImpl validaImpl = new ValidaURLImpl();
+
+    @Mock
+    private ConstraintValidatorContext contexto;
+
+    @Test
+    void deveReconhecerComoURLValida() {
+        String url = "https://www.google.com";
+        boolean isValida = validaImpl.isValid(url, contexto);
+        Assertions.assertTrue(isValida);
+    }
+
+    @Test
+    void deveReconhecerComoURLComQueryParamsValida() {
+        String url = "https://www.google.com/search?q=test";
+        boolean isValida = validaImpl.isValid(url, contexto);
+        Assertions.assertTrue(isValida);
+    }
+
+    @Test
+    void deveReconhecerComoURLHTTPValida() {
+        String url = "http://www.google.com";
+        boolean isValida = validaImpl.isValid(url, contexto);
+        Assertions.assertTrue(isValida);
+    }
+
+    @Test
+    void deveReconhecerComoURLSemProtocoloInvalida() {
+        String url = "www.google.com";
+        boolean isValida = validaImpl.isValid(url, contexto);
+        Assertions.assertFalse(isValida);
+    }
+
+    @Test
+    void deveReconhecerComoURLSemPontoComInvalida() {
+        String url = "https://www.google/search?q=test";
+        boolean isValida = validaImpl.isValid(url, contexto);
+        Assertions.assertFalse(isValida);
+    }
+}
