9d7a7144198503c93ae78a44b66bbcb7f0b90215
Marcos Camargo <mp.camargo@outlook.com>
2025-12-13 16:03:29 -0300

    test: Testes unitários do Snowflake
    - Implementados os seguintes testes para o gerador,
    nos seguintes casos:
      - Gerar um identificador com sucesso
      - Verificar se ocorre exceção quando o aluguel
      do container não está válido
      - Verificiar se ocorre exceção se o relógio do
      sistema atrasou
      - Verificar se o gerador irá esperar para gerar
      outro timestamp nos seguintes casos
        - timestampAtual == ultimoTimestampGerado
        - sequencia == 0

---
+++ backend/encurta-url/src/test/java/com/encurtaurl/principal/ut/snowflake/SnowflakeTests.java
@@ -0,0 +1,85 @@
+package com.encurtaurl.principal.ut.snowflake;
+
+import com.encurtaurl.principal.api.utils.Snowflake;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.util.ReflectionTestUtils;
+import java.time.Instant;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class SnowflakeTests {
+
+    @Mock
+    private StringRedisTemplate redisTemplate;
+
+    @Mock
+    private ValueOperations<String, String> valueOperations;
+
+    private Snowflake gerador;
+
+    @BeforeEach
+    void configurar() throws Exception {
+        // 1. O aluguel do container sempre estará ativo
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(valueOperations.setIfAbsent(any(), any(), anyLong(), any())).thenReturn(true);
+
+        // 2. Constrói o objeto Snowflake
+        this.gerador = new Snowflake(redisTemplate);
+
+        // 3. Configura a última renovação do aluguel do container
+        long timestamp = ReflectionTestUtils.invokeMethod(gerador, "obterTimestamp");
+        ReflectionTestUtils.setField(gerador, "ultimoTimestampAluguelRenovado", new AtomicLong(timestamp));
+    }
+
+    @Test
+    void deveGerarIdentificadorComSucesso() {
+        Assertions.assertThatNoException().isThrownBy(() -> gerador.gerarId());
+    }
+
+    @Test
+    void deveGerarExcecaoPoisGeradorEstaAtrasado() {
+        long timestampAdiantadoUmaHora = (long) ReflectionTestUtils.invokeMethod(gerador, "obterTimestamp")
+                + 360_000;
+        ReflectionTestUtils.setField(gerador, "ultimoTimestampGerado", timestampAdiantadoUmaHora);
+        Assertions.assertThatException().isThrownBy(() -> gerador.gerarId());
+    }
+
+    @Test
+    void deveGerarExcecaoPoisAluguelNaoEValido() {
+        ReflectionTestUtils.setField(gerador, "aluguelValido", new AtomicBoolean(false));
+        Assertions.assertThatException().isThrownBy(() -> gerador.gerarId()).isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void deveEsperarParaGerarOutroTimestamp() {
+        long ultimoTimestampGerado = (long) ReflectionTestUtils.invokeMethod(gerador, "obterTimestamp");
+        ReflectionTestUtils.setField(gerador, "ultimoTimestampGerado", ultimoTimestampGerado);
+        long timestampReferencia = (long) ReflectionTestUtils.getField(gerador, "timestampReferencia");
+
+        Instant primeiroInstante = Instant.ofEpochMilli(timestampReferencia + ultimoTimestampGerado);
+        Instant novoInstante = primeiroInstante.plusMillis(1);
+        try (MockedStatic<Instant> mockedStatic = Mockito.mockStatic(Instant.class)) {
+            mockedStatic.when(Instant::now).thenReturn(primeiroInstante).thenReturn(novoInstante);
+
+            long id = 0;
+            try {id = gerador.gerarId();} catch (Exception ex) {}
+
+            long timestampNovo = id >> 22;
+            org.junit.jupiter.api.Assertions.assertNotEquals(0, timestampNovo);
+            org.junit.jupiter.api.Assertions.assertNotEquals(ultimoTimestampGerado, timestampNovo);
+        }
+    }
+}
