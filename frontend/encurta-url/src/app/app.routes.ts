4464e5620aa3933c6691c8aa6c6ec3eb4971edf7
Alysson Silva <josealysson3010@gmail.com>
2025-12-25 16:59:22 -0300

    feat: Adicionada página Home e incluído campo de input
      - Criado componente que representa a Home, incluido título e campo de
        input para o link que será encurtado.
    
      - Refatorado componente Logo para melhorar sua reutilização.
    
      - Incluido efeito hover ao passsar o mouse no Logo.

--- frontend/encurta-url/src/app/app.routes.ts
+++ frontend/encurta-url/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 
-export const routes: Routes = [
-    {path: "/", component: Home},
-];
+export const routes: Routes = [{ path: '', component: Home }];
