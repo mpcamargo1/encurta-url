3de85e5733a1fb504e123e89e5b2e95b487d6985
Marcos Camargo <mp.camargo@outlook.com>
2026-03-16 12:38:24 -0300

    feat: Implementada estrutura do projeto (QRCode)

---
+++ backend/qrcode/src/main/java/com/encurtaurl/qrcode/controller/QRCodeController.java
@@ -0,0 +1,27 @@
+package com.encurtaurl.qrcode.controller;
+
+import com.encurtaurl.qrcode.model.qrcode.QRCodeRequest;
+import com.encurtaurl.qrcode.model.qrcode.QRCodeResponse;
+import com.encurtaurl.qrcode.service.QRCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class QRCodeController {
+
+    @Autowired
+    private QRCodeService service;
+
+    @PostMapping("/qrcode")
+    public ResponseEntity<QRCodeResponse> codificarEmQRCode(@RequestBody QRCodeRequest request) throws Exception {
+        QRCodeResponse response = new QRCodeResponse();
+        return ResponseEntity.of(Optional.of(response));
+    }
+
+}
