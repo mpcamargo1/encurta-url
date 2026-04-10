6965254f23f86c611e619b1ebcf77d80f0e5c6b2
Marcos Camargo <mp.camargo@outlook.com>
2026-04-10 17:10:42 -0300

    fix(qrcode): IndexOutOfBounds ao intercalar os blocos
    - Ajustado o parâmetro 'tamanhobloco' do PolinomioGerador. O valor a ser
      preenchido deve conter o tamanho por bloco, ao invés do tamanho total
      do bloco.
    
    - Corrigido cálculo do tamanho do array do bloco intercalado. Devido ao
      modo que estava, uma IndexOutOfBounds era disparada ao realizar a
      intercalação para QRCodes superiores a versão 3.

--- backend/qrcode/src/main/java/com/encurtaurl/qrcode/utils/reedsolomon/polinomiogerador/PolinomioGeradorUtils.java
+++ backend/qrcode/src/main/java/com/encurtaurl/qrcode/utils/reedsolomon/polinomiogerador/PolinomioGeradorUtils.java
@@ -15,21 +15,15 @@ public class PolinomioGeradorUtils {
         mapaPolinomioPorVersao.put(3, new PolinomioGerador(26, 1, 44,
                 new int[] {1, 246, 51, 183, 4, 136, 98, 199, 152, 77, 56, 206, 24, 145, 40, 209, 117, 233, 42, 135, 68,
                         70, 144, 146, 77, 43, 94}));
-        mapaPolinomioPorVersao.put(4, new PolinomioGerador(18, 2, 64,
+        mapaPolinomioPorVersao.put(4, new PolinomioGerador(18, 2, 32,
                 new int[] {1, 239, 251, 183, 113, 149, 175, 199, 215, 240, 220, 73, 82, 173, 75, 32, 67, 217, 146}));
-        mapaPolinomioPorVersao.put(5, new PolinomioGerador(24, 2, 86,
+        mapaPolinomioPorVersao.put(5, new PolinomioGerador(24, 2, 43,
                 new int[] {1, 122, 118, 169, 70, 178, 237, 216, 102, 115, 150, 229, 73, 130, 72, 61, 43, 206, 1, 237,
                         247, 127, 217, 144, 117}));
-        mapaPolinomioPorVersao.put(6, new PolinomioGerador(16, 4, 108,
+        mapaPolinomioPorVersao.put(6, new PolinomioGerador(16, 4, 27,
                 new int[] {1, 59, 13, 104, 189, 68, 209, 30, 8, 163, 65, 41, 229, 98, 50, 36, 59}));
-        mapaPolinomioPorVersao.put(7, new PolinomioGerador(18, 4, 136,
+        mapaPolinomioPorVersao.put(7, new PolinomioGerador(18, 4, 34,
                 new int[] {1, 239, 251, 183, 113, 149, 175, 199, 215, 240, 220, 73, 82, 173, 75, 32, 67, 217, 146}));
-        mapaPolinomioPorVersao.put(8, new PolinomioGerador(22, 4, 154,
-                new int[] {1, 89, 179, 131, 176, 182, 244, 19, 189, 69, 40, 28, 137, 29, 123, 67, 253, 86, 218, 230, 26,
-                        145, 245}));
-        mapaPolinomioPorVersao.put(9, new PolinomioGerador(22, 5, 182,
-                new int[] {1, 89, 179, 131, 176, 182, 244, 19, 189, 69, 40, 28, 137, 29, 123, 67, 253, 86, 218, 230, 26,
-                145, 245}));
     }
 
     public static PolinomioGerador getPolinomioGerador(int versao) throws Exception {
