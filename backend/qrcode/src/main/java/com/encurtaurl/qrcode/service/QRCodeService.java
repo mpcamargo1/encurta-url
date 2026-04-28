9f9d427d1f132c868a964ff2ac43ad1b70a63f21
Marcos Camargo <mp.camargo@outlook.com>
2026-04-28 18:42:39 -0300

    feat(qrcode): Comunicação GRPC no QRCode
    - Adicionadas dependências no pom.xml.
    
    - Adicionadas propriedades no application.properties.
    
    - Implementado service de encurtamento de URL.
    
    - Alterado QRCodeService para encurtar a URL antes de
      codificar em um QRCode.

--- backend/qrcode/src/main/java/com/encurtaurl/qrcode/service/QRCodeService.java
+++ backend/qrcode/src/main/java/com/encurtaurl/qrcode/service/QRCodeService.java
@@ -1,13 +1,17 @@
 package com.encurtaurl.qrcode.service;
 
 import com.encurtaurl.qrcode.utils.QRCodeUtils;
-import jakarta.validation.constraints.NotBlank;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QRCodeService {
 
-    public byte[] criarQRCode(@NotBlank String url) throws Exception {
-        return QRCodeUtils.gerarQRCode(url);
+    @Autowired
+    public EncurtaURLService encurtaURLService;
+
+    public byte[] criarQRCode(String url) throws Exception {
+        String urlEncurtada = encurtaURLService.encurtar(url);
+        return QRCodeUtils.gerarQRCode(urlEncurtada);
     }
 }
