91560d777cb1c8fca1fff96887bbd2da50d8abbf
Marcos Camargo <mp.camargo@outlook.com>
2026-06-13 14:20:23 -0300

    fix(run-app): Falha ao executar o script start.sh
    - O script start.sh falhava quando é iniciado fora do
    diretório run-app (ex: ./run-app/start.sh).
    
    - Para corrigir o problema, foi necessária a declaração
    de uma variável de referência para obter o caminho do
    diretório completo do arquivo de script.

--- run-app/start.sh
+++ run-app/start.sh
@@ -1,4 +1,6 @@
-cd ../frontend/encurta-url
+PASTA_REFERENCIAL_RUN_APP=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+cd ${PASTA_REFERENCIAL_RUN_APP} && cd ../frontend/encurta-url
 
 ng build --configuration development
 
@@ -7,12 +9,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-rm -rfv ../../run-app/frontend
+cd ${PASTA_REFERENCIAL_RUN_APP} && rm -rfv frontend && mkdir frontend
 
-mkdir ../../run-app/frontend
-
-cp -v -rp dist/encurta-url/browser/* ../../run-app/frontend
-
-cd ../../run-app
+cd ../frontend/encurta-url && cp -v -rp dist/encurta-url/browser/* ../../run-app/frontend && cd ${PASTA_REFERENCIAL_RUN_APP}
 
 docker compose up --build
