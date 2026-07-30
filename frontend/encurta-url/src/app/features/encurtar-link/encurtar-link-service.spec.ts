47e5151cdf9c778b6f2ca5ff7f2e9870a20f87d2
Alysson Silva <josealysson3010@gmail.com>
2026-07-30 19:09:32 -0300

    refactor: Refatorada lógica do formulário
      - Refatorada lógica do componente de formulário
        para passar o comportamento de envio de
        requisição para o service.
    
      - Refatorado arquivo de testes do service.

--- frontend/encurta-url/src/app/features/encurtar-link/encurtar-link-service.spec.ts
+++ frontend/encurta-url/src/app/features/encurtar-link/encurtar-link-service.spec.ts
@@ -2,17 +2,46 @@ import { TestBed } from '@angular/core/testing';
 
 import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { computed, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Message } from '../../shared/components/message-box/Message';
+import { MESSAGES } from '../../shared/components/message-box/MESSAGES';
+import { MessagesService } from '../../shared/components/message-box/messages-service';
 import { EncurtarLinkService } from './encurtar-link-service';
 import { EncurtarLinkRequest } from './EncurtarLinkRequest';
+import { EncurtarLinkResponse } from './EncurtarLinkResponse';
 
 describe('EncurtarLinkService', () => {
   let service: EncurtarLinkService;
   let httpMock: HttpTestingController;
 
+  let messagesService: {
+    messageSignal: ReturnType<typeof signal<Message | null>>;
+    message: any;
+    addMessage: any;
+    clearMessage: any;
+  };
+
   beforeEach(() => {
+
+    messagesService = {
+      messageSignal: signal(null),
+
+      get message() {
+        return computed(() => this.messageSignal());
+      },
+
+      addMessage: vi.fn((val: Message) => messagesService.messageSignal.set(val)),
+      clearMessage: vi.fn(() => messagesService.messageSignal.set(null)),
+    };
+    
     TestBed.configureTestingModule({
-      providers: [EncurtarLinkService, provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        EncurtarLinkService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {provide: MessagesService, useValue: messagesService}
+      ],
     });
 
     service = TestBed.inject(EncurtarLinkService);
@@ -28,16 +57,82 @@ describe('EncurtarLinkService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('deve montar a requisição para encurtar link corretamente', () => {
+  it('deve encurtar o link corretamente', () => {
     const request: EncurtarLinkRequest = { longUrl: 'https://youtube.com' };
 
-    service.encurtarURL(request).subscribe();
+    service.encurtarURL(request);
+
+    expect(service.waitingResponse()).toBe(true);
 
     const urlEsperada = `${environment.baseApiUrl}/shorten`;
 
     const req = httpMock.expectOne(urlEsperada);
 
     expect(req.request.method).toBe('POST');
-    req.flush({});
+
+    const resp: EncurtarLinkResponse = {
+      shortUrl: 'https://encurtaurl.net/1lT0Z1MeWW',
+      originalUrl: request.longUrl,
+      createdAt: ''
+    };
+
+    req.flush(resp);
+
+    expect(messagesService.addMessage).toHaveBeenCalledWith(MESSAGES.LINK_SUCCESS('https://encurtaurl.net/1lT0Z1MeWW'));
+    expect(service.waitingResponse()).toBe(false);
+  });
+
+  it('deve tratar timeout', () => {
+    const request: EncurtarLinkRequest = { longUrl: 'https://youtube.com' };
+
+    service.encurtarURL(request);
+
+    expect(service.waitingResponse()).toBe(true);
+
+    const urlEsperada = `${environment.baseApiUrl}/shorten`;
+
+    const req = httpMock.expectOne(urlEsperada);
+
+    expect(req.request.method).toBe('POST');
+
+    const error = new Error('TimeoutError');
+
+    error.name = 'TimeoutError';
+
+    req.flush(error,
+      {
+        status: 504,
+        statusText: 'TimeoutError'
+      });
+
+    expect(messagesService.addMessage).toHaveBeenCalledWith(MESSAGES.API_TIMEOUT_ERROR());
+    expect(service.waitingResponse()).toBe(false);
+  });
+
+  it('deve tratar erro inesperado', () => {
+    const request: EncurtarLinkRequest = { longUrl: 'https://youtube.com' };
+
+    service.encurtarURL(request);
+
+    expect(service.waitingResponse()).toBe(true);
+
+    const urlEsperada = `${environment.baseApiUrl}/shorten`;
+
+    const req = httpMock.expectOne(urlEsperada);
+
+    expect(req.request.method).toBe('POST');
+
+    const error = new Error('Error');
+
+    error.name = 'Error';
+
+    req.flush(error,
+      {
+        status: 500,
+        statusText: 'Error'
+      });
+
+    expect(messagesService.addMessage).toHaveBeenCalledWith(MESSAGES.API_UNEXPECTED_ERROR());
+    expect(service.waitingResponse()).toBe(false);
   });
 });
