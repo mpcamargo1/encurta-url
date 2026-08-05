d62e34683615c9918a1034781d463ca7417dc2df
Alysson Silva <josealysson3010@gmail.com>
2026-08-05 19:25:32 -0300

    feat: Implementada geração de QR Code no Front-end   - Adicionado envio da request para QR Code pelo     Front-end.
      - Adicionada a exibição do QR Code no Message
        Box.
    
      - Adicionado teste unitário para a geração do
        QR Code.

---
+++ frontend/encurta-url/src/app/features/encurtar-link/EncurtarLinkToQrCodeRequest.ts
@@ -0,0 +1,3 @@
+export interface EncurtarLinkToQrCodeRequest {
+    url: string,
+}
--- frontend/encurta-url/src/app/features/encurtar-link/encurtar-link-service.ts
+++ frontend/encurta-url/src/app/features/encurtar-link/encurtar-link-service.ts
@@ -6,13 +6,15 @@ import { MESSAGES } from '../../shared/components/message-box/MESSAGES';
 import { MessagesService } from '../../shared/components/message-box/messages-service';
 import { EncurtarLinkRequest } from './EncurtarLinkRequest';
 import { EncurtarLinkResponse } from './EncurtarLinkResponse';
+import { EncurtarLinkToQrCodeRequest } from './EncurtarLinkToQrCodeRequest';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EncurtarLinkService {
   private baseApiUrl = environment.baseApiUrl;
-  private apiUrl = `${this.baseApiUrl}/shorten`;
+  private shortenUrl = `${this.baseApiUrl}/shorten`;
+  private qrCodeApiUrl = `${this.baseApiUrl}/qrcode`
 
   private httpClient = inject(HttpClient);
 
@@ -20,11 +22,13 @@ export class EncurtarLinkService {
 
   readonly waitingResponse = signal<boolean>(false);
 
+  private qrCodeImgUrl: string | null = null;
+
   encurtarURL(request: EncurtarLinkRequest) {
 
     this.waitingResponse.set(true);
 
-    this.httpClient.post<EncurtarLinkResponse>(this.apiUrl, request)
+    this.httpClient.post<EncurtarLinkResponse>(this.shortenUrl, request)
       .pipe(
         timeout(30000),
         finalize(() => this.waitingResponse.set(false)),
@@ -32,6 +36,40 @@ export class EncurtarLinkService {
       .subscribe({
         next: (res) => {
           this.messagesService.addMessage(MESSAGES.LINK_SUCCESS(res.shortUrl));
+        },
+        error: (err) => {
+          let errorMessage = MESSAGES.API_UNEXPECTED_ERROR();
+
+          const errorName = err.error?.name || err.name;
+
+          if (errorName === 'TimeoutError') {
+            errorMessage = MESSAGES.API_TIMEOUT_ERROR();
+          }
+
+          this.messagesService.addMessage(errorMessage);
+        },
+      });;
+  }
+
+  encurtarURLParaQrCode(request: EncurtarLinkToQrCodeRequest) {
+
+    this.waitingResponse.set(true);
+
+    this.httpClient.post(this.qrCodeApiUrl, request, { responseType: 'blob' })
+      .pipe(
+        timeout(30000),
+        finalize(() => this.waitingResponse.set(false)),
+      )
+      .subscribe({
+        next: (res) => {
+          // Limpeza de memória do ObjectURL antigo
+          if (this.qrCodeImgUrl) {
+            URL.revokeObjectURL(this.qrCodeImgUrl);
+          }
+
+          this.qrCodeImgUrl = URL.createObjectURL(res);
+
+          this.messagesService.addMessage(MESSAGES.QRCODE_SUCCESS(this.qrCodeImgUrl));
         },
         error: (err) => {
           let errorMessage = MESSAGES.API_UNEXPECTED_ERROR();
